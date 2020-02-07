abfcd83b586f6adfb2b05c970cac7270a05bab7f
zurukumo <zurukumo@gmail.com>
2020-02-07 18:20:48 +0900

    update

--- oversample.py
+++ oversample.py
@@ -1,7 +1,8 @@
-in_file = 'sample1-sp-10-inf.csv'
-COUNT = 20000
+COMB = 2
+LEN = 10
+in_file = 'sample' + str(COMB) + '-sp-' + str(LEN) + '-inf.csv'
+COUNT = 40000
 
-import pandas as pd
 import csv
 
 cnt = [[COUNT] * 2 for _ in range(34)]
@@ -16,7 +17,7 @@ for row in f :
   x = row[34:]
 
   for i in range(34) :
-    with open('sample1-sp-10-' + str(i) + '.csv', 'a') as f :
+    with open('sample' + str(COMB) + '-sp-' + str(LEN) + '-' + str(i) + '.csv', 'a') as f :
       writer = csv.writer(f)
       if t[i] == 0 and cnt[i][0] > 0 :
         writer.writerow([0] + x)
