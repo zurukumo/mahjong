4d2da384a124f3d708e4d7f83afe6a0311efaf18
zurukumo <zurukumo@gmail.com>
2021-10-03 14:44:10 +0900

    refactor: 使われていない汎用関数を削除

--- server/game_base.py
+++ server/game_base.py
@@ -3,19 +3,5 @@ class GameBase:
         pass
 
     # 汎用関数
-    def find_player(self, player_id):
-        for player in self.players:
-            if player.id == player_id:
-                return player
-
-    def make_simple(self, original):
-        if original == 16:
-            return 35
-        if original == 52:
-            return 36
-        if original == 88:
-            return 37
-        return original // 4
-
     def prange(self):
         return [self.players[i % 4] for i in range(self.teban, self.teban + 4)]
