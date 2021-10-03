ed239c52702edfbe83562901a08a6e6c2ed2b1b9
zurukumo <zurukumo@gmail.com>
2021-10-03 14:13:42 +0900

    feat: メッセージ機能削除

--- server/player.py
+++ server/player.py
@@ -1,8 +1,7 @@
 from .player_action import PlayerAction
 from .player_base import PlayerBase
-from .player_message import PlayerMessage
 from .player_judge import PlayerJudge
 
 
-class Player(PlayerAction, PlayerBase, PlayerJudge, PlayerMessage):
+class Player(PlayerAction, PlayerBase, PlayerJudge):
     pass
