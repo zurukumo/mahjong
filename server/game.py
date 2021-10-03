60f277706b66f4808ea6ca44a6f1bf318d42bb95
zurukumo <zurukumo@gmail.com>
2021-10-03 14:53:53 +0900

    feat: デバッグ機能追加

--- server/game.py
+++ server/game.py
@@ -5,5 +5,8 @@ from .game_routine import GameRoutine
 
 
 class Game(GameAction, GameRoutine):
+    def __init__(self, debug=False):
+        self.debug = debug
+
     def prange(self):
         return [self.players[i % 4] for i in range(self.teban, self.teban + 4)]
