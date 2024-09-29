77bd690a6d8a86664b51481d8aec2a918fd12d06
zurukumo <zurukumo@gmail.com>
2024-09-29 20:48:13 +0900

    fix: max_caseの初期値を設定

--- paihu_parse.py
+++ paihu_parse.py
@@ -415,7 +415,7 @@ class PaihuParser():
 
 
 if __name__ == '__main__':
-    max_case = int(questionary.text('Max Case?').ask())
     mode = questionary.select('Mode?', choices=[mode.value for mode in Mode]).ask()
+    max_case = int(questionary.text('Max Case?', default='150000').ask())
 
     PaihuParser(mode=Mode(mode), max_case=max_case, debug=False)
