66c78b28375ddd81283dc3e765647337d5545562
zurukumo <zurukumo@gmail.com>
2024-10-20 14:25:56 +0900

    fix: serverの機能はkago-utilsに移す予定なので削除

--- server/player.py
+++
@@ -1,7 +0,0 @@
-from .player_action import PlayerAction
-from .player_base import PlayerBase
-from .player_judge import PlayerJudge
-
-
-class Player(PlayerAction, PlayerBase, PlayerJudge):
-    pass
